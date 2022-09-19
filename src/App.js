86bd0123a39e5f59474390caf5124d81c7d380a3
Luke <lukegothic@gmail.com>
2022-09-19 14:29:05 +0200

    mi primer custom hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { UserPreferences } from './services/UserPreferences';
-import { Index, Loading } from "./views";
+import { useAppData } from 'views/_functions/Hooks';
+import { Index, Loading } from "views";
 
 const App = () => {
-    const [appData, setAppData] = useState(null);
-    useEffect(() => {
-        (async () => {
-            const preferences = UserPreferences.read();
-            const i18n = await fetch(`conf/i18n/${preferences.lang}/ui.json`);
-            setAppData({ preferences, i18n });
-        })();
-      }, []);
+    const { appData } = useAppData();
     return (
         <>{ appData ? <Index /> : <Loading />} </>
     );
